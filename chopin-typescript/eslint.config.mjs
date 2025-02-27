9258b91d4e56564b28693e9220c4cd6448d38955
lzrs <lzrs@modular.cloud>
2025-02-27 13:45:55 -0800

    Monkeypatch speakeasy

--- chopin-typescript/eslint.config.mjs
+++ chopin-typescript/eslint.config.mjs
@@ -10,6 +10,13 @@ export default [
   ...tseslint.configs.recommended,
   {
     rules: {
+      /**
+       * Monkey Patch
+       */
+      "no-useless-escape": "off",
+      /**
+       * End of Monkey Patch
+       */
       "no-constant-condition": "off",
       // Handled by typescript compiler
       "@typescript-eslint/no-unused-vars": "off",
--- chopin-typescript/src/lib/config.ts
+++ chopin-typescript/src/lib/config.ts
@@ -39,8 +39,8 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "0.1.0",
-  sdkVersion: "0.0.1",
+  sdkVersion: "0.0.2",
   genVersion: "2.533.0",
   userAgent:
-    "speakeasy-sdk/typescript 0.0.1 2.533.0 0.1.0 @chopinframework/sdk",
+    "speakeasy-sdk/typescript 0.0.2 2.533.0 0.1.0 @chopinframework/sdk",
 } as const;
